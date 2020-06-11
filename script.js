473f6e8d8d2c0845252e352bdfd0e646c240eaa8
LLenroc <nc_2@hotmail.com>
2020-06-10 22:46:12 -0400

    created value for numbner slide, script js

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ const characterAmountNumber = document.getElementById
 ('characterAmountNumber')
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
-
 characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+function syncCharacterAmount(e) {
+    const value = e.target.value
+    characterAmountNumber.value = value
+    characterAmountRange.value = value
+}
