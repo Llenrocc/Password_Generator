d3bdd76da62158bbd36f86a806e146346e6eb917
LLenroc <nc_2@hotmail.com>
2020-06-11 12:44:52 -0400

    adding numbers,symbols, uppercase elements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var characterAmountRange = document.getElementById('characterAmountRange');
 var characterAmountNumber = document.getElementById('characterAmountNumber');
+var includeUppercaseElement = document.getElementById('includeUppercase');
 var form = document.getElementById('generatePasswordForm');
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount);
@@ -7,12 +8,16 @@ characterAmountRange.addEventListener('input', syncCharacterAmount);
 
 
 form.addEventListener('submit', e  => {
-    e.preventDefault()
+    e.preventDefault();
+    var characterAmount = characterAmountNumber.value;
+    var includeUppercase = includeUppercaseElement.checked;
+    var includeNumbers = includeNumbersElement.checked;
+    var includeSymbols = includeSymbolsElement.checked;
     var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
 });
 
 function syncCharacterAmount(e) {
     var value = e.target.value
-    characterAmountNumber.value = value
-    characterAmountRange.value = value
+    characterAmountNumber.value = value;
+    characterAmountRange.value = value;
 }
