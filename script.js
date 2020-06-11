2a292ccb99d55d6310cf4815108cde0f559c9810
LLenroc <nc_2@hotmail.com>
2020-06-10 23:04:44 -0400

    adding variables/value to labvel inputs

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const characterAmountRange = document.getElementById
 
 const characterAmountNumber = document.getElementById 
 ('characterAmountNumber')
+const includeUppercaseElement = document.getElementById
+('includeUppercase')
 const form = document.getElementById('generatePassword')
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
@@ -10,6 +12,9 @@ characterAmountRange.addEventListener('input', syncCharacterAmount)
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const password = generatePassword(characterAmount, includeUppercase,
+    includeNumbers, includeSymbols)
 })
 
 function syncCharacterAmount(e) {
