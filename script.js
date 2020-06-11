370407213fd472db1adc7caaae8203edc98874fb
LLenroc <nc_2@hotmail.com>
2020-06-11 11:51:16 -0400

    number slider is working again

--- script.js
+++ script.js
@@ -1,71 +1,11 @@
-const characterAmountRange = document.getElementById 
-('characterAmountRange')
-const characterAmountNumber = document.getElementById 
-('characterAmountNumber')
-const includeUppercaseElement = document.getElementById
-('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('generatePassword')
-const passwordDisplay = document.getElementById('passwordDisplay')
+var characterAmountRange = document.getElementById('characterAmountRange');
+var characterAmountNumber = document.getElementById('characterAmountNumber');
 
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58, 64)
-).concat(
-    arrayFromLowToHigh(91, 96)
-).concat(
-    arrayFromLowtoHigh(123, 126)
-)
-
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const characterAmount = characterAmountNumber.value
-    const includeUppercase = includeUppercaseElement.checked
-    const includeNumbers = includeNumbersElement.checked
-    const includSymbols = includeSymbolsElement.checked
-
-    const password = generatePassword(characterAmount, includeUppercase,
-    includeNumbers, includeSymbols)
-    passwordDisplay.innerText = password
-
-})
-
-function generatePassword(characterAmount, includeUppercase, includeNumbers,
-    includeSymbols) {
-        let charChodes = LOWERCASE_CHAR_CODES
-        if (includeUppercase) chardChodes = charChodes.concat
-        (UPPERCASE_CHAR_CODES)
-        if (includeSymbols) charCodes = charCodes.concat
-        (SYMBOL_CHAR_CODES)
-        if (includeNumbers) charCodes = charCodes.concat
-        (NUMBER_CHAR_CODES)
-
-        for (let i = 0; i < characterAmount; i++) {
-            const characterCode = charCodes[Math.floor(Math.random() *
-            charCodes.length)]
-            passwordCharacters.push(String.fromCharCode(characterCode))
-        }
-        return password.Characters.join()
-        }
-
-
-function arrayFromLowtoHigh(low, high) {
-    const array = []
-    for (let i = low; i <= high; i++) {
-    array.push
-}
-    return array
-}
+characterAmountNumber.addEventListener('input', syncCharacterAmount);
+characterAmountRange.addEventListener('input', syncCharacterAmount);
 
 function syncCharacterAmount(e) {
-    const value = e.target.value
+    var value = e.target.value
     characterAmountNumber.value = value
     characterAmountRange.value = value
 }
-
