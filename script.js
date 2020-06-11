27bf0bbf6929941900b70ff50defd0e7fa2defd3
LLenroc <nc_2@hotmail.com>
2020-06-11 12:30:00 -0400

    page does not refresh upon click of generate password button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var characterAmountRange = document.getElementById('characterAmountRange');
 var characterAmountNumber = document.getElementById('characterAmountNumber');
+var form = document.getElementById('generatePasswordForm');
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount);
 characterAmountRange.addEventListener('input', syncCharacterAmount);
+
+
+form.addEventListener('submit', e  => {
+    e.preventDefault()
+    var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+});
 
 function syncCharacterAmount(e) {
     var value = e.target.value
